4aab925c7bd77029214dd0ba22ce532667b9877e
s961206 <s961206@gmail.com>
2021-03-05 14:45:47 -0500

    add all tests

--- run_test.bat
+++ run_test.bat
@@ -4,36 +4,36 @@ javac -cp ".\libs\*" -d bin -s bin -h bin @sources.txt
 del "output\*.out"
 
 
-REM REM REM run test case 1: one peer to be a buyer of fish and another to be a seller of fish. Ensure that all fish is sold and restocked forever.
-REM 
-REM  echo b 0 fish 1 1 > info-id-0
-REM  echo s 1 fish 0 1 > info-id-1
-REM  
-REM  start cmd /k java -cp ".\bin;.\libs\*" roles.Person 1
-REM  start cmd /k java -cp ".\bin;.\libs\*" roles.Person 0
-REM  TIMEOUT 7
-REM  taskkill /F /IM java.exe
-REM  
-REM REM REM run test case 2:  one peer to be a buyer of fish and another to be a seller of boar. Ensure that nothing is sold.
-REM 
-REM  echo b 0 fish 1 1 > info-id-0
-REM  echo s 1 boars 0 1 > info-id-1
-REM  start cmd /k java -cp ".\bin;.\libs\*" roles.Person 1
-REM  start cmd /k java -cp ".\bin;.\libs\*" roles.Person 0
-REM  TIMEOUT 7
-REM  taskkill /F /IM java.exe
-REM 
-REM REM run test case 3: Randomly assign buyer and seller roles. Ensure that items keep being sold throughout.
-REM 
-REM  echo na 0 na 1 1 > info-id-0
-REM  echo na 1 na 0 1 > info-id-1
-REM  start cmd /k java -cp ".\bin;.\libs\*" roles.Person 1
-REM  start cmd /k java -cp ".\bin;.\libs\*" roles.Person 0
-REM  TIMEOUT 10
-REM  taskkill /F /IM java.exe
+REM REM run test case 1: one peer to be a buyer of fish and another to be a seller of fish. Ensure that all fish is sold and restocked forever.
+
+ echo b 0 fish 1 1 > info-id-0
+ echo s 1 fish 0 1 > info-id-1
+ 
+ start cmd /k java -cp ".\bin;.\libs\*" roles.Person 1
+ start cmd /k java -cp ".\bin;.\libs\*" roles.Person 0
+ TIMEOUT 7
+ taskkill /F /IM java.exe
+ 
+REM REM run test case 2:  one peer to be a buyer of fish and another to be a seller of boar. Ensure that nothing is sold.
+
+ echo b 0 fish 1 1 > info-id-0
+ echo s 1 boars 0 1 > info-id-1
+ start cmd /k java -cp ".\bin;.\libs\*" roles.Person 1
+ start cmd /k java -cp ".\bin;.\libs\*" roles.Person 0
+ TIMEOUT 7
+ taskkill /F /IM java.exe
+
+REM run test case 3: Randomly assign buyer and seller roles. Ensure that items keep being sold throughout.
+
+ echo na 0 na 1 1 > info-id-0
+ echo na 1 na 0 1 > info-id-1
+ start cmd /k java -cp ".\bin;.\libs\*" roles.Person 1
+ start cmd /k java -cp ".\bin;.\libs\*" roles.Person 0
+ TIMEOUT 10
+ taskkill /F /IM java.exe
 
 
-REM run test case 4:
+REM run test case 4: One seller of boar, 3 buyers of boars, the remaining peers have no role. Fix the neighborhood structure so that buyers and sellers are 2-hop away in the peer-to-peer overlay network.
  echo b 0 boars 4 1 test4 > info-id-0
  echo s 1 boars 4,5 1 test4 > info-id-1
  echo b 2 boars 5 1 test4 > info-id-2
