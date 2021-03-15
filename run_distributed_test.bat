68765595c7dfaa98e78d586032b5d478b07de506
s961206 <s961206@gmail.com>
2021-03-15 06:25:40 -0400

    fix bug

--- run_distributed_test.bat
+++ run_distributed_test.bat
@@ -183,7 +183,7 @@ for /f "tokens=*" %%a in (ids.txt) do (
   aws ec2 terminate-instances --instance-ids %%a
 )
 aws ec2 delete-key-pair --key-name 677kp32144321
-del 677kp3214432132144321.pem
+del 677kp32144321.pem
 del instance.json
 del ids.txt
 del ips.txt
